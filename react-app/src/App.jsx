d146cf574a40236743910c2b8cc6f1223dd34578
Gari <garikworkaddress@gmail.com>
2025-11-10 13:15:26 +0100

    instalacion y agregacion de rutas WASAAAAAAAAAAAAAAAAAAA

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header.jsx'
+import './App.css';
+import {Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-<Header />
-      <div>
-        <h1>Papa's Pizzeria To Go</h1>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+  
     </>
-  )
+  );
 }
 
-export default App
+export default App;
