904cce18dea3167990f8e7aab2d36781b516e55a
Eizmendi38 <eizmendiiker8@gmail.com>
2025-11-10 14:12:56 +0100

    Revert "instalacion y agregacion de rutas"
    This reverts commit d146cf574a40236743910c2b8cc6f1223dd34578.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,18 +1,20 @@
-import './App.css';
-import {Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Header from "./components/Header";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header.jsx'
 
 function App() {
- 
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-  
+<Header />
+      <div>
+        <h1>Papa's Pizzeria To Go</h1>
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
