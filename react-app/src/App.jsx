f140eee7b6413e153671c15f8d5b73d5ac008858
Eizmendi38 <eizmendiiker8@gmail.com>
2025-11-10 12:56:58 +0100

    header

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+<Header />
       <div>
         <h1>Papa's Pizzeria To Go</h1>
       </div>
