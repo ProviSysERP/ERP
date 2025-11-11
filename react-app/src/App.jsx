28d727c086c59866241d8026fd01092fd35fa2bb
Eizmendi38 <eizmendiiker8@gmail.com>
2025-11-11 11:51:43 +0100

    voy a empezar a cambiar el componente de novedades

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
 import Header from './components/Header.jsx'
+import Novedades from './components/Novedades.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-<Header />
-      <div>
-        <h1>Papa's Pizzeria To Go</h1>
-      </div>
-    </>
+    <Box sx={{ width: '100%', minHeight: '100vh' }}>
+      <Header />
+      <Container maxWidth="lg">
+        <Novedades />
+        <Box sx={{ py: 4 }}>
+          <h1></h1>
+        </Box>
+      </Container>
+    </Box>
   )
 }
 
