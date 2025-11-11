3cb113b76f40e086e636ab95d2c685deb631ae03
Eizmendi38 <eizmendiiker8@gmail.com>
2025-11-11 10:38:08 +0100

    Cambio

---
+++ index.js
@@ -0,0 +1,95 @@
+import {app, BrowserWindow, Menu} from 'electron';
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 700,
+        height: 900,
+    });
+    //win.loadFile('index.html');
+    win.loadURL('http://localhost:5173/')
+    //win.webContents.openDevTools();
+
+    win.on('maximize', () => console.log('Window maximized'));
+    win.on('minimize', () => console.log('Window minimized'));
+    win.on('focus', () => console.log('Window focused'));
+    win.on('blur', () => console.log('Window lost focus'));
+
+    const templateDefault = [
+     {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Menu',
+          click: () => {
+            win.loadFile('menu.html');
+          },
+        },
+        {
+          label: 'Pedidos',
+          click: () => {
+            win.loadFile('pedidos.html');
+          },
+        },
+        { type: 'separator' },
+        { role: 'quit', label: 'Salir' },
+      ],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'About Us', click: () => console.log('About Us clicked') },
+      ],
+    },
+  ];
+  
+
+    // definir MENU
+     const templateMac = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Menu',
+          click: () => {
+            win.loadFile('menu.html');
+          },
+        },
+        {
+          label: 'Pedidos',
+          click: () => {
+            win.loadFile('pedidos.html');
+          },
+        },
+        { type: 'separator' },
+        { role: 'quit', label: 'Salir' },
+      ],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'About Us', click: () => console.log('About Us clicked') },
+      ],
+    },
+  ];
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+  let template;
+  if (process.platform === 'darwin') {
+    template = templateMac;
+  } else {
+    template = templateDefault;
+  }
+};
+
+app.whenReady().then(() => {
+    createWindow();
+});
+app.on('window-all-closed', () => {
+    console.log('All windows closed');
+    app.quit();
+});
+app.whenReady().then(() => {
+  console.log(process.platform);
+});
