bb4a33883b4eb96f21912ab4944fe39faafc8531
Eizmendi38 <eizmendiiker8@gmail.com>
2025-11-12 09:32:44 +0100

    Cambio img

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Header from './components/Header.jsx'
-import Novedades from './components/Novedades.jsx'
+import Contactos from './pages/Contactos.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,11 +12,12 @@ function App() {
     <Box sx={{ width: '100%', minHeight: '100vh' }}>
       <Header />
       <Container maxWidth="lg">
-        <Novedades />
+        <Contactos />
         <Box sx={{ py: 4 }}>
           <h1></h1>
         </Box>
       </Container>
+      
     </Box>
   )
 }
