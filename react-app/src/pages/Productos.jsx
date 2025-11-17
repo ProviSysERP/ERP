6f8d38ef2e76fc0c5fabeecd8c8f7ac5f6084f6d
JON-CEBANC <jon.cebancuser@gmail.com>
2025-11-17 13:08:58 +0100

    cambio productos

--- react-app/src/pages/Productos.jsx
+++ react-app/src/pages/Productos.jsx
@@ -52,7 +52,7 @@ export default function Productos() {
             Productos
         </Typography>
 
-        <Box sx={{ display: 'grid', gridTemplateColumns: { xs: '1fr', sm: '1fr', md: '1fr 1fr' }, gap: 3 }}>
+        <Box sx={{ display: 'grid', gridTemplateColumns: { xs: '1fr', sm: '1fr 1fr', md: 'repeat(4, 1fr)' }, gap: 3 }}>
             {products && products.length > 0 ? (
             products.map((product) => (
                 <Card key={product.id_product} sx={{ maxWidth: 345 }}>
