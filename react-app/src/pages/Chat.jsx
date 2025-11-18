99d6bbebdc5ceeec82b61425de91336f6bd70a8b
Gari <garikworkaddress@gmail.com>
2025-11-18 13:30:05 +0100

    CLANKER DOWN

--- react-app/src/pages/Chat.jsx
+++ react-app/src/pages/Chat.jsx
@@ -1,3 +1,20 @@
+import { useEffect } from 'react';
 import Header from '../components/Header.jsx'
+import { obtenerUsuario } from '../components/ObtenerUsuario.js'
+
+
+
 export default function Chat() {
+
+    const usuario = obtenerUsuario();
+    const [isLoading, setIsLoading] = useState(true);
+    const usID = usuario.id_user;
+
+    useEffect(() => {
+        setIsLoading(true);
+        
+    }, []);
+
+
+
 }
