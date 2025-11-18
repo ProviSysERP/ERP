2e53435c0aa5275e0e8f91e306350ce4cb3c33d1
Odei <odei.cebanc@gmail.com>
2025-11-18 12:46:59 +0100

    Arregladofaltabaunpunto

--- react-app/src/components/ProtectedRoute.jsx
+++ react-app/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { obtenerUsuario } from "./components/ObtenerUsuario";
+import { obtenerUsuario } from "../components/ObtenerUsuario.js";
 
 export default function ProtectedRoute({ children }) {
   const navigate = useNavigate();
